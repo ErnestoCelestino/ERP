b5c96eefaa47ae994f4c04d1237df00638adc07d
ernesto <ernesto@debian.ernesto>
2021-04-11 10:52:59 -0300

    Configura estrutura do projeto

---
+++ erp-autentication/src/main/java/br/com/erp/ErpAutenticationApplication.java
@@ -0,0 +1,13 @@
+package br.com.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAutenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAutenticationApplication.class, args);
+	}
+
+}
